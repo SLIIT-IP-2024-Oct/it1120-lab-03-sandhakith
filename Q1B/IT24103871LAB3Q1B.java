f4cca9d94bf53c5bbc3c93ddec9c1a0acf57f191
sandhakith <it24103871@my.sliit.lk>
2024-11-14 16:57:08 +0530

    Add files via upload

---
+++ Q1B/IT24103871LAB3Q1B.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class IT24103871LAB3Q1B {
+
+public static void main (String[] args)
+ {
+ Scanner input= new Scanner (System.in);
+
+
+
+  double kilos,kgs_price,total;
+
+  System.out.println("1kgs_price");
+  kgs_price = input.nextDouble();
+
+  System.out.println("how much kilos u want");
+   kilos= input.nextDouble();
+
+   total =kilos*kgs_price;
+  
+   
+   System.out.println(total);
+
+
+   
+  
+
+
+}
+
+}
